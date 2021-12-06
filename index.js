ccb110c5894c9dff5e8cba2051922fcf441afe71
Jerry-007 <kshitij2022000@gmail.com>
2021-12-06 16:59:08 +0530

    first version

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const cors = require("cors");
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors());
+const PORT = process.env.PORT || 8000;
+
+const users = {};
+
+const socketToRoom = {};
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("joinedRoom", (roomId) => {
+    if (users[roomId]) {
+      users[roomId].push(socket.id);
+    } else {
+      users[roomId] = [socket.id];
+    }
+
+    socketToRoom[socket.id] = roomId;
+    const usersInRoom = users[roomId].filter((id) => id !== socket.id);
+
+    socket.emit("users", usersInRoom);
+
+    socket.on("sendingSignals", (data) => {
+      io.to(data.userToSignal).emit("userJoined", {
+        signal: data.signal,
+        callerId: data.callerId,
+      });
+    });
+
+    socket.on("returningSignal", (data) => {
+      io.to(data.callerId).emit("receivedReturnedSignal", {
+        signal: data.signal,
+        id: socket.id,
+      });
+    });
+
+    socket.on("disconnect", () => {
+      const roomID = socketToRoom[socket.id];
+      let room = users[roomID];
+      if (room) {
+        room = room.filter((id) => id !== socket.id);
+        users[roomID] = room;
+      }
+    });
+
+  });
+});
+
+server.listen(PORT, () => console.log(`Server Listening on PORT ${PORT} `));
